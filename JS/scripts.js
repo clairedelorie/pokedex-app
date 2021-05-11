d7b71af54c88cf4dc59f96872490a03ad2c2705d
clairedelorie <delorie.claire@gmail.com>
2021-05-11 19:52:42 -0400

    Update scripts.js
    added in 3 pokemon to the list

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,6 @@
-alert('Hello world');
+// blank array for Pokemon data
+let pokemonList= [{name: "Bulbasaur", height: "0.7 m", type: ["Grass","Poison"]},{name: "Ivysaur", height: "1 m", type: ["Grass","Poison"]},{name: "Venusaur", height: "2m", type: ["Grass","Poison"]}];
 
-let favoriteFood = 'Ramen';
-document.write(favoriteFood);
 
 
 
