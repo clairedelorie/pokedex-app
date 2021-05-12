6708bb5073249f4c22f9d215a77cf8fa13481a05
clairedelorie <delorie.claire@gmail.com>
2021-05-11 20:03:07 -0400

    Update scripts.js
    edited layout for better reading

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,8 @@
 // blank array for Pokemon data
-let pokemonList= [{name: "Bulbasaur", height: "0.7 m", type: ["Grass","Poison"]},{name: "Ivysaur", height: "1 m", type: ["Grass","Poison"]},{name: "Venusaur", height: "2m", type: ["Grass","Poison"]}];
+let pokemonList= [{name: "Bulbasaur", height: 0.7, type: ["Grass","Poison"]},
+                  {name: "Ivysaur", height: 1, type: ["Grass","Poison"]},
+                  {name: "Venusaur", height: 2, type: ["Grass","Poison"]}
+                ];
 
 
 
