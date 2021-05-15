ca33b2460447d9223c2b6c8aa2efdefe333e42e9
clairedelorie <delorie.claire@gmail.com>
2021-05-14 22:34:36 -0400

    Update scripts.js
    added for loop and edited layout

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,28 @@
 // blank array for Pokemon data
-let pokemonList= [{name: "Bulbasaur", height: 0.7, type: ["Grass","Poison"]},
-                  {name: "Ivysaur", height: 1, type: ["Grass","Poison"]},
-                  {name: "Venusaur", height: 2, type: ["Grass","Poison"]}
-                ];
+let pokemonList = [
+    {
+      name: "Bulbasaur", 
+      height: 0.7, 
+      type: ["grass","poison"]
+    },
+    {
+      name: "Ivysaur", 
+      height: 1, 
+      type: ["grass","poison"]
+    },
+    {
+      name: "Venusaur", 
+      height: 2, 
+      type: ["grass","poison"]
+    }
+];
 
 
-
-
-
-
+// for loop
+for (let i=0; i < pokemonList.length; i++){
+    if(pokemonList[i].height >=2){
+        document.write(pokemonList[i].name + pokemonList[i].height + " Wow! That's big!")
+    }else{
+        document.write(pokemonList[i].name + pokemonList[i].height)
+    }
+}
