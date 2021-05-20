01180f5accd487794d011e344ad3216145af72a1
clairedelorie <delorie.claire@gmail.com>
2021-05-19 22:42:17 -0400

    Update scripts.js
    changed repository

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,34 +1,40 @@
 // blank array for Pokemon data
-let pokemonList = [
-    {
-      name: "Bulbasaur", 
-      height: 0.7, 
-      type: ["grass","poison"]
-    },
-    {
-      name: "Ivysaur", 
-      height: 1, 
-      type: ["grass","poison"]
-    },
-    {
-      name: "Venusaur", 
-      height: 2, 
-      type: ["grass","poison"]
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+        name: "Bulbasaur", 
+        height: 0.7, 
+        type: ["grass","poison"]
+        },
+        {
+        name: "Ivysaur", 
+        height: 1, 
+        type: ["grass","poison"]
+        },
+        {
+        name: "Venusaur", 
+        height: 2, 
+        type: ["grass","poison"]
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+        }
+
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    return {
+        add: add,
+        getAll: getall
+    };
+})();
 
 
 // for loop
-// for (let i=0; i < pokemonList.length; i++){
-  //  if(pokemonList[i].height >= 2){
-    //    document.write(pokemonList[i].name + " (" + "height " + pokemonList[i].height + ") " + "- Wow thats big! <br />");
-    //}else{
-      //  document.write(pokemonList[i].name + " (" + "height " + pokemonList[i].height + ") <br />");
-    //}
-//}
-//pokemonList.forEach(function(name, height, type) {
-  //  document.write(pokemonList.name + " (" + "height " + pokemonList.height + ") <br />");
-//});
+
 function myLoopFunction(pokemon) {
     document.write(pokemon.name + " (" + "height " + pokemon.height + ") <br />");
 }
