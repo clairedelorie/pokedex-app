f5eaea14ce75ecda0d799f297b265a2a662ca0a8
clairedelorie <delorie.claire@gmail.com>
2021-05-24 21:01:54 -0400

    Update scripts.js
    edited show details function

--- JS/scripts.js
+++ JS/scripts.js
@@ -46,7 +46,7 @@ function addListItem(pokemon) {
 }
 
 function showDetails(pokemon) {
-    loadDetails(pokemon).then(function(event) {
+    loadDetails(pokemon).then(function() {
 		console.log(pokemon);
 	});
 }
